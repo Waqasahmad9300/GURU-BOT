fda0e715245765a1015b2df667b98165116c5e90
Waqasahmad9300 <108908697+Waqasahmad9300@users.noreply.github.com>
2023-12-27 22:13:03 +0500

    Update config.js

--- config.js
+++ config.js
@@ -54,13 +54,13 @@ global.botname = 'Kashii 💕 Mishii'
 global.premium = 'true'
 global.packname = 'Kashii 💕 Mishii' 
 global.author = '@Asliguru' 
-global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
+global.menuvid = 'https://telegra.ph/file/2bf0d6a4d4ca54d524465.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
 global.fgyt = 'https://youtube.com/@Asliguru'
 global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
+global.fglog = 'https://telegra.ph/file/8e013402ac9008841affb.jpg' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 
